852940c93081d12c84ea713bb3364fb1f4efb4cc
nabilmousquetaire <37777623+nabilmousquetaire@users.noreply.github.com>
2024-04-15 13:28:09 +0200

    Todo

---
+++ accor/src/main/java/com/accor/infrastructure/Todo.java
@@ -0,0 +1,4 @@
+package com.accor.infrastructure;
+
+public class Todo {
+}
---
+++ accor/src/test/java/com/accor/infrastructure/ToDoTests.java
@@ -0,0 +1,13 @@
+package com.accor.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
